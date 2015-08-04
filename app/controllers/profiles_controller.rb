f848d49cc379a77226280b19d57dcf841d16f055
David Alvarado <davidealva@gmail.com>
2015-08-03 22:15:42 -0600

    updated devise routes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,20 +3,7 @@ class ProfilesController < ApplicationController
 	before_action :only_current_user
 
 	def new
-		# form for user to fill out new profile
-		@user = User.find(params[:user_id])
-		@profile = Profile.new
-	end
-
-	def create
-		@user = User.find(params[:user_id])
-		@profile = @user.build_profile(profile_params)
-		if @profile.save
-			flash[:success] = "Your profile has been created"
-			redirect_to user_path(params[:user_id])
-		else
-			render action: :new 
-		end
+		redirect_to artist_path(current_user)
 	end
 
 	def edit
